92395eaecc026b468875357dcbc4db517ece2c81
20120612 <nguyenlamtruong20021@gmail.com>
2023-05-16 13:49:30 +0700

    fix file host

--- blast_tool_kit/src/host/index.js
+++ blast_tool_kit/src/host/index.js
@@ -1,6 +1,4 @@
-const SERVER_API = 'https://test-blast.onrender.com/api';
+export const SERVER_API = 'https://test-blast.onrender.com/api';
 // const SERVER_API = 'http://localhost:8000/api';
-const AUTHEN =
+export const AUTHEN =
     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NTI3YjcwZmZiYTQ3MTQzYTg5NzgzMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MzU2Nzk5NCwiZXhwIjoxNjg0MTcyNzk0fQ.Vr-BKCi-q9MBadfqovMBU2hEJgprsqpZ9eNhw6F-dtY';
-
-export default { SERVER_API, AUTHEN };
