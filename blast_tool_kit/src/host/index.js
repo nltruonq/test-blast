abdf35d91b5c33d7adee76eede72b103ff4e986c
20120612 <nguyenlamtruong20021@gmail.com>
2023-05-16 14:05:27 +0700

    atk

--- blast_tool_kit/src/host/index.js
+++ blast_tool_kit/src/host/index.js
@@ -1,4 +1,4 @@
 export const SERVER_API = 'https://test-blast.onrender.com/api';
 // const SERVER_API = 'http://localhost:8000/api';
 export const AUTHEN =
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NjMyOTNkNmY4MTZjMDk4ZTRjN2M0OCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4NDIyMDM1MywiZXhwIjoxNjg0ODI1MTUzfQ.O-n48HrsxmkzhSfS792uCxLjMEJSpYrC-SefgqsO_nw';
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NjMyOTNkNmY4MTZjMDk4ZTRjN2M0OCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4NDIyMDYxMSwiZXhwIjoxNjg0ODI1NDExfQ.NNxN86Ez2NXU-42zoio7U4B7hyCpYfz6qfqEp7YJ_98';
