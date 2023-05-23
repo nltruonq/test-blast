5f6043d1251329cc87f7a0c51eb150013103e404
20120612 <nguyenlamtruong20021@gmail.com>
2023-05-23 17:44:10 +0700

    host

--- blast_tool_kit/src/host/index.js
+++ blast_tool_kit/src/host/index.js
@@ -1,2 +1,2 @@
-// export const SERVER_API = 'https://test-blast.onrender.com/api';
-export const SERVER_API = 'http://localhost:8000/api';
+export const SERVER_API = 'https://test-blast.onrender.com/api';
+// export const SERVER_API = 'http://localhost:8000/api';
