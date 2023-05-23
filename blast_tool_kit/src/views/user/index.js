4e8721b7ae87a10668210464d81c59f04775f17a
20120612 <nguyenlamtruong20021@gmail.com>
2023-05-23 23:34:26 +0700

    page size user update

--- blast_tool_kit/src/views/user/index.js
+++ blast_tool_kit/src/views/user/index.js
@@ -364,7 +364,7 @@ const User = () => {
                     columns={columnsInfoUser}
                     initialState={{
                         pagination: {
-                            paginationModel: { page: 0, pageSize: 5 }
+                            paginationModel: { page: 0, pageSize: 10 }
                         }
                     }}
                     pageSizeOptions={[5, 10]}
