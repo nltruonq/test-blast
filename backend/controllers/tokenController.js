10e4d9926d9b59946f9312fb5ec6e304a72c27ae
20120612 <nguyenlamtruong20021@gmail.com>
2023-05-30 22:12:30 +0700

    get token

--- backend/controllers/tokenController.js
+++ backend/controllers/tokenController.js
@@ -4,7 +4,7 @@ const tokenController = {
     get: async (req, res) => {
         try {
             const query = req.query;
-            const tokens = await Token.find();
+            const tokens = await Token.find({});
             return res.status(200).json(tokens);
         } catch (err) {
             return res.status(500).json(err.message);
@@ -13,21 +13,19 @@ const tokenController = {
     calculate: async (req, res) => {
         try {
             const query = req.query;
-            const tokens = await Token.find({query});
+            const tokens = await Token.find({ query });
 
-            const init = {
-
-            }
+            const init = {};
 
             const result = tokens.reduce((acc, cur) => {
-                if(cur.promptTokens && cur.completionTokens) {
-                    return acc 
+                if (cur.promptTokens && cur.completionTokens) {
+                    return acc;
                 }
-            }, 0)
-        } catch(err) {
+            }, 0);
+        } catch (err) {
             return res.status(500).json(err.message);
         }
-    }
+    },
 };
 
 module.exports = tokenController;
